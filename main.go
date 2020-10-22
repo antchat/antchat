6996676158ff5a6d2ae3cebfcfe25af31f1fd4a7
Richard <quanlongwang@gmail.com>
2020-10-22 21:57:12 +0800

    add wsServer

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ var addr = flag.String("addr", ":8080", "http server address")
 func main() {
     flag.Parse()
 
+	wsServer := NewWebsocketServer()
+	go wsServer.Run()
+
     http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
         ServeWs(w, r)
     })
